bc1faf813541b844ac57caf6ad9241e5769de684
YoujoSenku <scl491455@gmail.com>
2026-05-19 12:47:16 +0700

    update

--- ys1/app/Http/Controllers/DashboardController.php
+++ ys1/app/Http/Controllers/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends Controller
         $totalBarang = Product::count();
         $barangTersedia = Product::where('is_active', 1)->count();
         $barangHabis = Product::where('is_active', 0)->count();
-        $nilaiStok = 'Rp ' . number_format(Product::sum('harga'), 0, ',', '.');
+        $nilaiStok = 'Rp ' . number_format(Product::sum('price'), 0, ',', '.');
         $barangTerbaru = Product::latest()->take(5)->get();
         return view('dashboard', compact(
             'totalBarang',
